d7e1cf6b969a39c5575bf32ecdb1d49476e6cb41
EdgarACarneiro <up201503784@fe.up.pt>
2017-02-19 03:24:24 +0000

    Started Ogre. Missing some small details and the club!

--- DungeonKeep/src/Board.java
+++ DungeonKeep/src/Board.java
@@ -28,40 +28,66 @@ public class Board {
 	};
 	
 	public final static int[][] guard_mov = { 
-			{0, -1}, {1, 0} , {1, 0} , {1, 0} , {1, 0} ,
-			{0, -1}, {0, -1}, {0, -1}, {0, -1}, {0, -1},
-			{0, -1}, {1, 0} , {0, 1} , {0, 1} , {0, 1} ,
-			{0, 1} , {0, 1} , {0, 1} , {0, 1} , {-1, 0},
-			{-1, 0}, {-1, 0}, {-1, 0}, {-1, 0}, {-1, 0}
+			{ 0, -1}, { 1,  0}, { 1,  0}, { 1,  0}, { 1,  0},
+			{ 0, -1}, { 0, -1}, { 0, -1}, { 0, -1}, { 0, -1},
+			{ 0, -1}, { 1,  0}, { 0,  1}, { 0,  1}, { 0,  1},
+			{ 0,  1}, { 0,  1}, { 0,  1}, { 0,  1}, {-1,  0},
+			{-1,  0}, {-1,  0}, {-1,  0}, {-1,  0}, {-1,  0}
 	};
 	
+	// Character's related variables
 	public static Hero hero;
 	public static Guard guard;
 	public static int guard_mov_counter = 0;
+	public static Ogre ogre;
 	
-	public static int[] key_pos = { 8, 7 };
-	public static int[][] victory_pos = {
-			{ 5, 0 },
-			{ 6, 0 }
+	//Variables configurations for all maps
+	public static int current_lvl = 1;
+	public static char[][][] maps = { map1, map2 };
+	
+	public static int[][] key_pos = { 
+			{ 8, 7 },
+			{ 1, 8 }
+	};
+	
+	public static int[][][] victory_pos = { 
+			{ { 5, 0 }, { 6, 0 } },
+			{ { 1, 0 } }
+	};
+	
+	public static int[][] hero_init = { 
+			{ 1, 1 },
+			{ 7, 1 }
 	};
 
+	
 	public static Scanner s = new Scanner(System.in);
 
 	public static enum State { WIN, LOSS, NONE };
 
 	public static void main(String[] args) {
-		hero = new Hero(new int[]{1, 1});
+		
+		hero = new Hero(hero_init[current_lvl]);
 		guard = new Guard(new int[] {1, 8});
+		ogre = new Ogre(new int[] {1, 4});
 				
 		do {
 			// Draw game state
-			char[][] tmp_board = copyBoard(map1);
+			char[][] tmp_board = copyBoard(maps[current_lvl]);
 			
 			hero.draw(tmp_board);
-			guard.draw(tmp_board);
 			
-			guard.move(guard_mov[guard_mov_counter++ % guard_mov.length]);
-			
+			switch (current_lvl) {
+			case 0:
+				guard.draw(tmp_board);
+				guard.move(guard_mov[guard_mov_counter++ % guard_mov.length]);
+				break;
+			case 1:
+				ogre.draw(tmp_board);
+				ogre.move(ogre.randomMove());
+				break;
+			}
+			 
 			printBoard(tmp_board);			
 			
 		} while (gameCycle() == State.NONE);
@@ -92,28 +118,46 @@ public class Board {
 	
 	public static State checkState() {
 		
-		// Check Lever
-		if (hero.pos[0] == key_pos[0] && hero.pos[1] == key_pos[1]) {
+		// Check Lever / Key
+		if (hero.pos[0] == key_pos[current_lvl][0] && hero.pos[1] == key_pos[current_lvl][1]) {
 			System.out.println("KEY FOUND");
-			for (int i = 0;  i < map1.length; ++i) {
-				for (int j = 0;  j < map1[i].length; ++j) {
-					if (map1[i][j] == 'I')
-						map1[i][j] = 'S';
+			for (int i = 0;  i < maps[current_lvl].length; ++i) {
+				for (int j = 0;  j < maps[current_lvl][i].length; ++j) {
+					if (maps[current_lvl][i][j] == 'I')
+						maps[current_lvl][i][j] = 'S';
 				}
 			}
 		}
 		
 		// Check Loss
-		if (hero.isNear(guard)) {
-			System.out.println("YOU LOST");
-			return State.LOSS;
+		switch (current_lvl) {
+		case 0:
+			if (hero.isNear(guard)) {
+				System.out.println("YOU LOST");
+				return State.LOSS;
+			}
+			break;
+		case 1:
+			if (hero.isNear(ogre)) {
+				System.out.println("YOU LOST");
+				return State.LOSS;
+			}
+			break;
 		}
 		
+		
 		// Check Win
-		for (int[] pos : victory_pos) {
+		for (int[] pos : victory_pos[current_lvl]) {
 			if (pos[0] == hero.pos[0] && pos[1] == hero.pos[1]) {
-				System.out.println("YOU WON!");
-				return State.WIN;
+				
+				if (current_lvl != 1) {
+					current_lvl++;
+					hero.pos = hero_init[current_lvl];
+				}
+				else {
+					System.out.println("YOU WON!");
+					return State.WIN;
+				}
 			}
 		}
 		
--- DungeonKeep/src/Ogre.java
+++ DungeonKeep/src/Ogre.java
@@ -1,8 +1,23 @@
+import java.util.Random;
 
 public class Ogre extends Character {
 
 	public Ogre(int[] initial_pos) {
 		super('O', initial_pos);
 	}
-
+	
+	public int[] randomMove() {
+		
+		//Possible movements
+		final int[][] random_mov = { 
+				{  0,  1},
+				{  0, -1},
+				{  1,  0},
+				{ -1,  0}				
+		};
+		
+		Random rand = new Random();
+		
+		return random_mov[rand.nextInt(4)];
+	}
 }
