34cccaddc84857b953a4fa0c06ad1ef16307e16e
EdgarACarneiro <up201503784@fe.up.pt>
2017-03-03 12:25:30 +0000

    Level Two stuff.

--- DungeonKeep/src/dkeep/logic/LevelTwo.java
+++ DungeonKeep/src/dkeep/logic/LevelTwo.java
@@ -1,15 +1,49 @@
 package dkeep.logic;
 
+import java.util.Random;
+
 public class LevelTwo extends Level {
 
+	private Random rand = new Random();
+	
+	private Hero hero;
+	int ogreNumber ;
+	private Ogre[] ogres;
+	private OgreMap map;
+	
 	public LevelTwo() {
-		// TODO Auto-generated constructor stub
+		map = new OgreMap();
+		hero = new Hero(map.hero_pos);
+		ogreNumber = rand.nextInt(3) + 2;
+		ogres = new Ogre[ogreNumber];
+		
+		//Initializing all the ogres in the same place
+		for (int i = 0; i < ogres.length; ++i)
+			ogres[i] = new Ogre(map.ogre_pos);
+		
 	}
 
 	@Override
 	public state update(int row, int col) {
-		// TODO Auto-generated method stub
-		return state.RUNNING;
+		
+		if (map.isValid(hero.pos[0]+row, hero.pos[1]+col))
+			hero.update(row, col);
+		
+		for (int i = 0; i < ogres.length; ++i) {
+			ogres[i].update(map);
+			
+			if (hero.isAdjacent(ogres[i])) {
+				System.out.println("You lost...");
+				return state.LOST;
+			}			
+		}
+		
+		if (! map.update(hero.pos)) {
+			return state.RUNNING;
+		} else {
+			System.out.println("You Won!!");
+			return state.WON;
+		}
 	}
 
 	@Override
@@ -20,7 +54,28 @@ public class LevelTwo extends Level {
 	
 	@Override
 	public void draw() {
+
+		// Creating a modifiable version of the map
+		char[][] map_copy = new char[map.getMap().length][map.getMap()[0].length];
 		
+		for (int i = 0; i < map.getMap().length; ++i)
+			for (int j = 0; j < map.getMap()[i].length; ++j)
+				map_copy[i][j] = map.getMap()[i][j];
+		
+		hero.draw(map_copy);
+		for (int i = 0; i < ogres.length; ++i)
+			ogres[i].draw(map_copy);
+		
+		// Printing the modified map
+		for (char[] s : map_copy) {
+			for (char c : s) {
+				if (c == 'B')
+					System.out.print("  ");
+				else
+					System.out.print(c + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
--- DungeonKeep/src/dkeep/logic/OgreMap.java
+++ DungeonKeep/src/dkeep/logic/OgreMap.java
@@ -1,9 +1,20 @@
 package dkeep.logic;
 
 // TODO
-public class OgreMap {
+public class OgreMap extends Map{
 
-	private final char[][] map = {
+	//private boolean doors_open = false;
+	
+	private static int[] key_pos = {1, 8};
+	
+	public static int[] ogre_pos = {1, 4};
+	
+	public static int[] hero_pos = {7, 1};	// initial hero position
+	
+	public OgreMap() {
+		super.setVictoryPos(new int[][] {{1, 0}});
+		
+		this.map = new char[][] {
 			{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
 			{'I', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'X'},
 			{'X', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'X'},
@@ -14,27 +25,18 @@ public class OgreMap {
 			{'X', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'X'},
 			{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
 		};
-
-	private boolean doors_open = false;
-	
-	private static int[] key_pos = {8, 7};
-	
-	private static int[][] victory_pos = {
-			{5, 0}, {6, 0}
-	};
-	
-	private static int[] hero_pos = {1, 1};	// initial hero position
-	
-	
-	public OgreMap() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public void openDoors() {
-		doors_open = true;
+		//doors_open = true;
 		
-		map[5][0] = 'S';
-		map[5][1] = 'S';
+		//map[1][0] = 'S';
 	}
+	
+	@Override
+	public boolean isValid(int row, int col) {
+		return (map[row][col] == 'B' || map[row][col] == 'S');
+	}
+
 
 }
