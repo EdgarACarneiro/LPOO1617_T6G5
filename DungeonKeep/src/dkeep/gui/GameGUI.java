6e8c8338c8a32ab0073f4092ac586389897f5f47
AndreFCruz <andrecruz97@gmail.com>
2017-03-10 11:09:01 +0000

    GUI Initial Commit

---
+++ DungeonKeep/src/dkeep/gui/GameGUI.java
@@ -0,0 +1,117 @@
+package dkeep.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+import javax.swing.DefaultComboBoxModel;
+
+public class GameGUI {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameGUI window = new GameGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GameGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNumberOfOgres = new JLabel("Number of Ogres");
+		lblNumberOfOgres.setFont(new Font("Malayalam MN", Font.PLAIN, 13));
+		lblNumberOfOgres.setBounds(22, 32, 137, 19);
+		frame.getContentPane().add(lblNumberOfOgres);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setText("#");
+		textField.setBounds(160, 27, 118, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblGuardPersonality = new JLabel("Guard Personality");
+		lblGuardPersonality.setFont(new Font("Malayalam MN", Font.PLAIN, 13));
+		lblGuardPersonality.setBounds(22, 63, 118, 16);
+		frame.getContentPane().add(lblGuardPersonality);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Rookie", "Drunken", "Suspicious"}));
+		comboBox.setToolTipText("Persona");
+		comboBox.setBounds(160, 59, 118, 27);
+		frame.getContentPane().add(comboBox);
+		
+		JButton btnNewGame = new JButton("New Game");
+		btnNewGame.setBounds(450, 79, 117, 29);
+		frame.getContentPane().add(btnNewGame);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(450, 407, 117, 29);
+		frame.getContentPane().add(btnExit);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Nadeem", Font.BOLD, 15));
+		textArea.setBounds(22, 105, 390, 300);
+		frame.getContentPane().add(textArea);
+		
+		JLabel lblNewLabel = new JLabel("Game Status Placeholder");
+		lblNewLabel.setFont(new Font("Malayalam MN", Font.PLAIN, 13));
+		lblNewLabel.setBounds(22, 417, 390, 19);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnUp = new JButton("Up");
+		btnUp.setBounds(475, 182, 75, 29);
+		frame.getContentPane().add(btnUp);
+		
+		JButton btnLeft = new JButton("Left");
+		btnLeft.setBounds(432, 223, 81, 29);
+		frame.getContentPane().add(btnLeft);
+		
+		JButton btnDown = new JButton("Down");
+		btnDown.setBounds(475, 264, 75, 29);
+		frame.getContentPane().add(btnDown);
+		
+		JButton btnRight = new JButton("Right");
+		btnRight.setBounds(511, 223, 75, 29);
+		frame.getContentPane().add(btnRight);
+	}
+}
